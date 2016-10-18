79ab04c8e02d1e7f444eb50760aa1089a00adb3f
BernardoM <bernardo.neto@sga.pucminas.br>
2016-10-18 13:28:36 -0200

    Programa portal

---
+++ portal/scroll.lua
@@ -0,0 +1,147 @@
+local posicao = 1
+local guarda_min = 1
+local guarda_max = 18
+--rever como calcular a partir das imagens do menu
+local distancia = 30
+local tamImagem = 180
+local dx, dy = canvas:attrSize()
+
+dofile("tbl_pgm.lua")
+
+local aux= {}
+local imagem = {}
+
+function criaVetor()
+    -- cria vetor primario
+   for i=guarda_min,guarda_max do
+      menu[i][canvas] = canvas:new('midia/menu2/'..tostring(i) .. 'off.png')
+      menu[i][dx]=(tamImagem *(i)  )
+    end
+    for i=guarda_min,guarda_max do
+        menu[i][dx] = menu[i][dx] - (tamImagem + distancia)
+    end
+
+end
+criaVetor()
+
+function clonaVetor()
+    -- cria vetor auxiliar
+    for i=guarda_min,guarda_max do
+      aux[i] = {}
+      aux[i][dx] = menu[i][dx]
+    end
+end
+clonaVetor()
+
+function restauraVetor()
+    -- cria vetor auxiliar
+    for i=guarda_min,guarda_max do
+      menu[i][dx] = aux[i][dx]
+    end
+end
+
+function desenha ()
+  -- fundo
+  canvas:attrColor('black')
+  canvas:drawRect('fill', 0, 0, dx, dy )
+
+  for i=guarda_min,guarda_max do   
+    canvas:compose(menu[i][dx] + distancia*(i) ,dy/5,menu[i][canvas])
+  end
+  
+  icone = canvas:new('midia/selecao.png')
+   fundo = canvas:new('midia/transparent2.png')
+
+   --canvas:attrColor('transparent')
+   --local tamanhoicone = 30
+  -- print(contador)
+  -- canvas:drawRect('frame', dx/1.8-(tamanhoicone/2), dy/1.48-(tamanhoicone/2), tamanhoicone, tamanhoicone)
+    canvas:compose(dx/1920,dy/5,fundo)
+     canvas:flush()
+   --canvas:attrColor('transparent')s
+   --local tamanhoicone = 30
+  -- print(contador)
+  -- canvas:drawRect('frame', dx/1.8-(tamanhoicone/2), dy/1.48-(tamanhoicone/2), tamanhoicone, tamanhoicone)
+
+    canvas:compose(dx/2.05,dy/1.40,icone)
+
+    esquerda = canvas:new('midia/esquerda.png')
+	canvas:compose(dx/100,dy/70,esquerda)
+    direita = canvas:new('midia/direita.png')
+    canvas:compose(dx/1.05,dy/70,direita)
+    canvas:flush()
+       end
+
+function goToFimVetor ()
+    for i=1,(guarda_max -5) do
+        for j=guarda_min,guarda_max do
+            menu[j][dx] = menu[j][dx] - (tamImagem + distancia)
+        end
+    end    
+end
+
+function writeText()
+  for i=guarda_min,posicao do
+        canvas:attrColor('black')
+        canvas:drawRect('fill', 0, 0, dx, dy )  
+        canvas:compose((dx/2 - dy/2)/5,dy/5,menu[posicao+2][canvas])
+        canvas:flush()
+    end
+         acima = canvas:new('midia/acima.png')
+         canvas:compose(dx/1920,dy/5,acima)
+         canvas:attrColor('maroon')
+      canvas:attrFont("vera", 22)
+      canvas:drawText(dx/2 -dy/1.1, dy/5, menu[posicao]["descricao"])
+      canvas:flush()
+end
+
+
+--desenha interface ao abrir
+desenha()
+
+function handler (evt)
+   if (evt.class == 'key' and evt.type == 'press') then      
+      if evt.key == "CURSOR_RIGHT" then
+          if posicao >= (guarda_max-4) then
+             posicao = guarda_min            
+             restauraVetor()
+          else
+              for i=guarda_min,guarda_max do
+                  menu[i][dx] = menu[i][dx] - (tamImagem + distancia)
+              end
+              posicao = posicao + 1
+              print("posicao "..posicao)
+          end          
+          desenha()
+      else
+        if evt.key == "CURSOR_LEFT" then
+          if posicao <= (guarda_min) then
+              posicao = guarda_max -4
+              restauraVetor()
+              goToFimVetor()                
+          else
+              for i=guarda_min,guarda_max do
+                  menu[i][dx] = menu[i][dx] + (tamImagem + distancia )
+                  canvas:flush()
+              end
+
+              posicao = posicao - 1
+             -- print("posicao "..posicao)
+          end              
+          desenha()
+      else
+        if evt.key == "ENTER" then
+            print ("pressionou enter, valor guarda:" .. tostring(posicao))
+              writeText() 
+            end
+          
+            if evt.key == "CURSOR_UP" then
+            print ("pressionou up, valor guarda:" .. tostring(posicao))
+             desenha()
+            end
+        end          
+      end
+  end
+end
+
+event.register(handler)
